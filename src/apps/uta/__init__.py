d069610a16bab2865800abaa70b4f6e98f746389
jacksho-vfx <trampishredman@gmail.com>
2025-10-09 05:33:42 +0100

    Align package versions at 1.0.0

--- src/apps/uta/__init__.py
+++ src/apps/uta/__init__.py
@@ -1,5 +1,6 @@
 """Web GUI for interacting with the OnePiece toolchain."""
 
+from .version import UTA_VERSION, __version__
 from .web import app
 
-__all__ = ["app"]
+__all__ = ["app", "UTA_VERSION", "__version__"]
