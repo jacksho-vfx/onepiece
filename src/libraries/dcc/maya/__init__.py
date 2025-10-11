b76e6c463042ccd7f71185631f9cd2d59139f0a3
jacksho-vfx <trampishredman@gmail.com>
2025-10-11 18:33:48 +0100

    Add Unreal export validation helper for Maya

--- src/libraries/dcc/maya/__init__.py
+++ src/libraries/dcc/maya/__init__.py
@@ -1,1 +1,23 @@
 """Maya specific DCC helpers."""
+
+from .unreal_export_checker import (  # noqa: F401
+    DEFAULT_ALLOWED_PREFIXES,
+    DEFAULT_EXPECTED_ROOT,
+    DEFAULT_EXPECTED_SCALE,
+    DEFAULT_REQUIRED_JOINTS,
+    DEFAULT_SCALE_TOLERANCE,
+    UnrealExportIssue,
+    UnrealExportReport,
+    validate_unreal_export,
+)
+
+__all__ = [
+    "DEFAULT_ALLOWED_PREFIXES",
+    "DEFAULT_EXPECTED_ROOT",
+    "DEFAULT_EXPECTED_SCALE",
+    "DEFAULT_REQUIRED_JOINTS",
+    "DEFAULT_SCALE_TOLERANCE",
+    "UnrealExportIssue",
+    "UnrealExportReport",
+    "validate_unreal_export",
+]
